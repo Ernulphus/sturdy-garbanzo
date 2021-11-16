d65b952192eb0399eea6613d3538222479470880
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-11-15 21:24:00 -0500

    work through starter code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'joe_shmo',
+        memberSince: '07/23/96'
+      }
+    }
+  }
+
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
+
   render() {
+
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
+    );
+    const LogInComponent = () => (<logIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
+
     return (
       <Router>
-        <div className="App">
-          Hello, World!
+        <div>
+          <Route exact path="/" render={HomeComponent}/>
+          <Route exact path = "/userProfile" render={UserProfileComponent}/>
+          <Route exact path="/login" render={LogInComponent}/>
         </div>
       </Router>
     );
   }
+
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
+
+class Home extends Component {
+  render(){
+    return (
+      <div>
+        <img src="https://cdn.iconscout.com/icon/free/png-256/bank-1417507-1201209.png" alt="bank"/>
+        <h1>Bank of React</h1>
+
+        <Link to="/userProfile">User Profile</Link>
+
+        <AccountBalance accountBalance={this.props.accountBalance}/>
+      </div>
+    );
+  }
+}
+
+export default Home;
