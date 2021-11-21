289344c7c45e36122be7b1988ed77b832dfdaa29
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-11-21 17:35:37 -0500

    finagle debits to display

--- bank-of-react/src/components/Debits.js
+++ bank-of-react/src/components/Debits.js
@@ -1,11 +1,15 @@
-const Debits = (props) =? {
-  let debitsView = () =? {
-    const {debits} = props;
+import React from 'react'
+
+const Debits = (props) => {
+
+  let debitsView = () => {
+    const { debits } = props;
     return debits.map((debit) => {
       let date = debit.date.slice(0,10);
       return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
     })
   }
+
   return (
     <div>
       <h1>Debits</h1>
