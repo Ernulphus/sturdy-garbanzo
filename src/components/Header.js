46103d29a4ac089fedd8f16d6e5e657b8df22307
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-11-22 17:07:29 -0500

    Add and style bank icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ class Header extends Component {
   render() {
     return (
       <div class="App-header">
+        <img src="https://cdn.iconscout.com/icon/free/png-256/bank-1417507-1201209.png" alt="bank"/>
         <Link to="/">Home </Link>
         <Link to="/userProfile">User Profile </Link>
         <Link to="/debits">Debits </Link>
