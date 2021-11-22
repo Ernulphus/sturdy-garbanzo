fc6b7c8538ad9abccab12297a5553f6c64e09a47
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-11-22 16:58:07 -0500

    Styled the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import {Link} from 'react-router-dom';
 class Header extends Component {
   render() {
     return (
-      <div>
+      <div class="App-header">
+        <Link to="/">Home </Link>
         <Link to="/userProfile">User Profile </Link>
         <Link to="/debits">Debits </Link>
         <Link to="/credits">Credits </Link>
